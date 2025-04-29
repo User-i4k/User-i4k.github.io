b401db781998c4ef6328af293c7d9bff17d1dc48
User-i4k <157296797+User-i4k@users.noreply.github.com>
2025-04-30 01:50:27 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-// app.js
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js';
 import { getDatabase } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js';
 
-// Firebase ayarları
 const firebaseConfig = {
   apiKey: "AIzaSyBxGSxaTj0J3lhnyQrwC9hDGs7-tKq600c",
   authDomain: "okul-sohbet-794d0.firebaseapp.com",
@@ -13,9 +11,7 @@ const firebaseConfig = {
   appId: "1:849190115812:web:70e78485af056296720ff1"
 };
 
-// Firebase uygulamasını başlat
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
-// Diğer dosyaların kullanabilmesi için dışa aktar
 export { db };
