acb2683e867cb63b99e6f288bf401364ca8704a3
User-i4k <157296797+User-i4k@users.noreply.github.com>
2025-10-23 00:28:18 +0300

    Update Firebase configuration settings
    Updated Firebase configuration with new project details.

--- app.js
+++ app.js
@@ -6,15 +6,27 @@ import { getDatabase, ref, set, onChildAdded } from "firebase/database";
 import { getAuth, signInAnonymously } from "firebase/auth";
 
 // Firebase yapılandırma ayarları
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    databaseURL: "YOUR_DB_URL",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyD6UDwZBwRJxPJjVo5NkoXUitBZ93jaqMc",
+  authDomain: "kayraokulu.firebaseapp.com",
+  databaseURL: "https://kayraokulu-default-rtdb.firebaseio.com",
+  projectId: "kayraokulu",
+  storageBucket: "kayraokulu.firebasestorage.app",
+  messagingSenderId: "1057965536521",
+  appId: "1:1057965536521:web:67d1bd5c0b0a76950a0fd8",
+  measurementId: "G-2TKESSQH6K"
 };
+
+// Initialize Firebase
+const analytics = getAnalytics(app);
 
 // Firebase'i başlat
 const app = initializeApp(firebaseConfig);
