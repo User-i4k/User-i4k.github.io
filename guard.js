f08461ad043fa6a7d3c8f2a5dd7bfab2d00e5af9
User-i4k <157296797+User-i4k@users.noreply.github.com>
2025-12-05 16:34:12 +0300

    Delete guard.js

--- guard.js
+++
@@ -1,9 +0,0 @@
-// Guard script to protect game.html and admin.html
-(() => {
-    const username = localStorage.getItem('username');
-    
-    if (!username) {
-        window.location.href = 'index.html';
-    }
-})();
-
